2e5382036d2c80fad617b898fbca391a6f6e59e6
nyx6965 <72759681+nyx6965@users.noreply.github.com>
2024-09-10 19:53:29 +0300

    tried adding css

--- hello_templ.go
+++ hello_templ.go
@@ -8,7 +8,7 @@ package main
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-func hello(name string) templ.Component {
+func hello() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
@@ -26,20 +26,7 @@ func hello(name string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div>Hello, ")
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		var templ_7745c5c3_Var2 string
-		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(name)
-		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `hello.templ`, Line: 4, Col: 19}
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
-		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<html lang=\"en\"><head><meta charset=\"UTF-8\"><title>snwy.me</title><style type=\"text/css\">\n\tbody {\n    width: 50%;\n    margin: 0 auto;\n    background: #151515;\n    font-family: \"Space Mono\", monospace;\n    font-size: 19px;\n    color: #fff;\n    line-height: 2.0;\n    display: table;\n}\n\nhtml,body\n{\n    height: 100%;\n}\n\nhtml::after {\n    content:'';\n    position: absolute;\n    top: 0;\n    width:100%;\n    height: 100%;\n    background-image: linear-gradient(rgba(0,0,0,0.4) 1px, transparent 1px);\n    background-size: 2px 2px;\n    background-repeat: repeat;\n    pointer-events: none;\n}\n\n@media screen and (max-width: 1300px) {\n    body {\n        width: 90%;\n    }\n}\n\nh1 {\n    font-size: 2.5em;\n    margin: 0;\n    padding: 0;\n}\n\nheader {\n    padding: 0 20px 20px 0;\n    display: flex;\n    align-items: center;\n}\n\nul {\n    padding-left: 0;\n    margin-top: 0;\n    list-style: none;\n}\n\na {\n    color: #fff;\n    text-decoration: white underline 2px;\n}\n\nhr {\n    height: 4px;\n    background: #fff;\n    opacity: 25%;\n    border: none;\n}\n\n#header_block {\n    margin-left: 5%;\n}\n\n#banner {\n    height: auto;\n    width: 35%;\n}\n\n#content {\n    height: 100%;\n    display: table-cell;\n    vertical-align: middle;\n    padding: 0;\n    margin: 0;\n}\n\t</style><style>\n    @import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');\n  </style><link rel=\"stylesheet\" href=\"./index.css\"></head><body><div id=\"content\"><header><img id=\"banner\" src=\"banner.jpg\"><div id=\"header_block\"><h1>snwy.me</h1><nav><ul><li>[<a href=\"https://github.com/snoglobe\">github</a>]</li><li>[<a href=\"https://x.com/snwy_me\">x</a>]</li><li>[<a href=\"https://www.youtube.com/@snwydotme\">youtube</a>]</li></ul></nav></div></header><hr><p>hey, i'm Mason Meirs - a.k.a. snwy. i'm a developer interested in programming language design and theory. i'm also interested in distributed systems, machine learning, and the future of computing.</p><p>i dropped out of high school in 2024 and got my GED to puruse a career.</p><p>i've built <a href=\"https://aurora-lang.org\">aurora</a>, a language built to be an automation tool that replaces the languages that came before it.</p><p></p><hr><p>contact me:</p><ul><li>[email: <a href=\"mailto:snwy@snwy.me\">snwy@snwy.me</a>]</li><li>[discord: snwy]</li></ul><hr><p>thanks for visiting!</p></div></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- main.go
+++ main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/a-h/templ"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 
-	component := hello("John")
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		templ.Handler(component)
-	})
-	http.ListenAndServe(":3000", r)
+	http.Handle("/", templ.Handler(hello()))
+	http.ListenAndServe(":8080", nil)
 }
