b0b91773efacce0ea4f6fdc71823605c894cfa4e
mertensnova <amr8644@gmail.com>
2025-02-25 14:32:04 +0300

    Added images

--- main.go
+++ main.go
@@ -11,7 +11,10 @@ import (
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+
+    r.Use(middleware.Logger)
+
+	r.Handle("/assets/*", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		component := view.Home()
 		component.Render(r.Context(), w)
